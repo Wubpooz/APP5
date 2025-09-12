9a65156c0b985c57594d50ed4ca08e28a31a6713
bluefir <mathieu.waharte@gmail.com>
2025-09-12 12:12:37 +0200

    [Qualite] TP2 Exo4

--- Qualite/TP2/Exo4/exo4.c
+++ Qualite/TP2/Exo4/exo4.c
@@ -1,26 +1,46 @@
 #include "exo4.h"
+#include <stdio.h>
 
 int max_element(const int* t, int n){
-	int max = 0;
+  int max = 0;
   /*@
     loop invariant 0 <= i <= n;
     loop invariant max >= 0 && max < n;
+    loop invariant \forall integer k; 0 <= k < n ==> t[k] == \at(t[k], Pre);
+    loop invariant \forall integer k; 0 <= k < i ==> t[k] <= t[max];
     loop assigns max, i;
     loop variant n - i;
-  */
-	for (int i = 0; i < n; i++){
-		if (t[max] < t[i]) max = i;
-	}
-	return max;
-}
+    */
+   for (int i = 0; i < n; i++){
+     if (t[max] < t[i]) max = i;
+    }
+    return max;
+  }
 
 void sum_of_tab(int *a, int *b, int n){
   /*@
     loop invariant 0 <= i <= n;
+    loop invariant \forall integer k; 0 <= k < n ==> b[k] == \at(b[k], Pre);
+    loop invariant \forall integer k; 0 <= k < i ==> a[k] == \at(a[k], Pre) + \at(b[k], Pre);
+    loop invariant \forall integer k; i <= k < n ==> a[k] == \at(a[k], Pre);
     loop assigns a[0 .. n-1], i;
     loop variant n - i;
   */
 	for (int i = 0; i<n; i++){
 		a[i] += b[i];
 	}
+}
+
+
+
+
+int main() {
+  int n = 5;
+  int a[6] = {1, 2, 3, 4, 5, 6};
+
+  sum_of_tab(a + 1, a, n);
+  for (int i = 0; i < n + 1; i++) {
+    printf("a[%d] = %d\n", i, a[i]);
+  }
+  return 0;
 }
--- Qualite/TP2/Exo4/exo4.h
+++ Qualite/TP2/Exo4/exo4.h
@@ -1,8 +1,9 @@
 #include "limits.h"
 
 /*@ 
-  requires n > 0;
+  requires 0 < n <= INT_MAX;
   requires \valid(t + (0 .. n - 1));
+  requires \separated(t + (0 .. n - 1), t);
   assigns t[0 .. n-1];
   ensures \forall int i; 0 <= i < n ==> t[i] <= t[\result];
   ensures \forall int i; 0 <= i < n ==> t[i] == \old(t[i]);
@@ -10,9 +11,11 @@
 int max_element(const int* t, int n);
 
 /*@
-  requires n > 0;
+  requires 0 < n <= INT_MAX;
   requires \valid(a + (0 .. n - 1));
   requires \valid(b + (0 .. n - 1));
+  requires \forall int i; 0 <= i < n ==> INT_MIN <= a[i] + b[i] <= INT_MAX;
+  requires \separated(a + (0 .. n-1), b + (0 .. n-1));
   assigns a[0 .. n-1];
   assigns b[0 .. n-1];
   ensures \forall int i; 0 <= i < n ==> a[i] == \old(a[i]) + b[i];
