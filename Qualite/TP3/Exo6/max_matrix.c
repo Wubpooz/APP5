e4dbe2a7bfac33a779599f31912114f7f1d9586f
bluefir <mathieu.waharte@gmail.com>
2025-09-23 19:59:10 +0200

    [Qualite] final

--- Qualite/TP3/Exo6/max_matrix.c
+++ Qualite/TP3/Exo6/max_matrix.c
@@ -1,24 +1,26 @@
 #include "max_matrix.h"
 
-int max_matrix(int **matrix, int n, int m)
-{
-	int i = 0;
-	int j = 0;
-	int max = matrix[0][0];
-	/*@ loop assigns i,j,max;
-	  @ loop invariant 0 <= i <= n;
-	  @ loop invariant 0 <= j < m;
-	*/
-	while (i < n)
-	{
-		if (max < matrix[i][j])
-			max = matrix[i][j];
-		j++;
-		if (j == m)
-		{
-			i++;
-			j = 0;
-		}
-	}
-	return max;
+int max_matrix(int **matrix, int n, int m) {
+  int i = 0;
+  int j = 0;
+  int max = matrix[0][0];
+  /*@ loop assigns i,j,max;
+    @ loop invariant 0 <= i <= n;
+    @ loop invariant 0 <= j < m;
+    @ loop invariant \forall integer k, l; 0 <= k < i && 0 <= l < m ==> max >= matrix[k][l];
+    @ loop invariant \forall integer l; 0 <= l <= j ==> max >= matrix[i][l];
+    @ loop invariant max == matrix[0][0] || \exists integer k, l;
+      ((0 <= k < i && 0 <= l < m) || (k == i && 0 <= l <= j)) && max == matrix[k][l];
+    @ loop variant n * m - (i * m + j);
+  */
+  while (i < n) {
+    if (max < matrix[i][j])
+      max = matrix[i][j];
+    j++;
+    if (j == m) {
+      i++;
+      j = 0;
+    }
+  }
+  return max;
 }
--- Qualite/TP3/Exo6/max_matrix.h
+++ Qualite/TP3/Exo6/max_matrix.h
@@ -7,5 +7,8 @@
   @ requires 0 < m <= INT_MAX;
   @ requires \valid(matrix + (0 .. n-1));
   @ requires \forall int k; 0 <= k <= n-1 ==> \valid(matrix[k]+ (0 .. m-1));
+  @ ensures \forall integer i,j; 0 <= i < n && 0 <= j < m ==> matrix[i][j] <= \result;
+  @ ensures \exists integer i,j; 0 <= i < n && 0 <= j < m && matrix[i][j] == \result;
+  @ assigns \nothing;
 */
 int max_matrix(int **matrix, int n, int m);
