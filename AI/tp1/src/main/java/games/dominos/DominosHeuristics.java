72046ffb14dd4270c9757aea78f337df32f84d20
Wubpooz <mathieu.waharte@gmail.com>
2026-04-08 16:49:45 +0200

    [AI] fix terminal condition

--- AI/tp1/src/main/java/games/dominos/DominosHeuristics.java
+++ AI/tp1/src/main/java/games/dominos/DominosHeuristics.java
@@ -7,7 +7,7 @@ public class DominosHeuristics {
   // if you have more moves than your opponent, you are in a better position, so the heuristic returns 2, in equal position it returns 1, and in a worse position it returns 0
 	public static IHeuristic<DominosBoard, DominosRole>  hVertical = (board, role) -> {
     if(board.isGameOver()) {
-      return role == DominosRole.VERTICAL && board.nbVerticalMoves() == 0 ? 99 : -99;
+      return role == DominosRole.VERTICAL && board.nbHorizontalMoves() == 0 ? 99 : -99;
     }
     if(board.nbHorizontalMoves() == board.nbVerticalMoves()) {
     	return 1;
@@ -22,7 +22,7 @@ public class DominosHeuristics {
 
 	public static IHeuristic<DominosBoard, DominosRole> hHorizontal = (board, role) -> {
     if(board.isGameOver()) {
-      return role == DominosRole.HORIZONTAL && board.nbHorizontalMoves() == 0 ? 99 : -99;
+      return role == DominosRole.HORIZONTAL && board.nbVerticalMoves() == 0 ? 99 : -99;
     }
     if(board.nbHorizontalMoves() == board.nbVerticalMoves()) {
     	return 1;
