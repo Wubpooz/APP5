5b5cead56018c7bf517a88660826da4edbd8d912
Wubpooz <mathieu.waharte@gmail.com>
2026-05-19 16:41:09 +0200

    [C++]TP2 noté, up to all_of

--- CPP/TP2_noté.cpp
+++ CPP/TP2_noté.cpp
@@ -1,24 +1,19 @@
-
 #include <iostream>
 #include <algorithm>
 #include <tuple>
+#include <type_traits>
 
 /*
-
   À la fin de la séance, envoyez le *Short link* de votre travail à l'adresse
   électronique qui vous a été donnée. Mettez en titre de votre email le nom de la séance,
   sous la forme :
-  
        ET5 - C++ Avancé - Travaux Pratiques n°... - NOM Prénom
 
   Comme contenu du message électronique :
-
        NOM Prénom - https://godbolt.org/z/...
-
 */
 
 /*
-
 On définit une **Séquence de Type** comme un *template* variadique de classe
 dont les paramètres *templates* encode le contenu d'une liste de types.
 
@@ -41,7 +36,6 @@ A chaque fois, des tests sont fournis, qui sont exécutable à la compilation.
 Il n'est pas nécessaire d'avoir une fonction main(), ni même de produire un
 binaire exécutable pour savoir si le code est correct, puisque tout est
 vérifié à la compilation par les `static_assert`.
-
 */
 
 template<typename... Types>
@@ -58,7 +52,7 @@ constexpr type_list<int,float,char[19], double, char, void**> test_tl;
 template<typename... Types>
 constexpr auto size(type_list<Types...>)
 {
-  return --- ;
+  return sizeof...(Types); //(j'imagine ceux qui n'ont vu que du JS lire ça...)
 }
 
 static_assert( size(null_tl) == 0 );
@@ -73,13 +67,13 @@ static_assert( size(test_tl) == 6 );
 template<typename... T1,typename... T2>
 constexpr auto operator==(type_list<T1...>, type_list<T2...>)
 {
-  return ---;
+  return std::is_same_v<type_list<T1...>, type_list<T2...>>; // is_same_v compare valeur par valeur, pas besoin de le refaire nous même, merci std
 }
 
 template<typename... T1,typename... T2>
 constexpr auto operator!=(type_list<T1...>, type_list<T2...>)
 {
-  return ---;
+  return !std::is_same_v<type_list<T1...>, type_list<T2...>>;
 }
 
 static_assert( test_tl == test_tl );
@@ -92,7 +86,7 @@ static_assert( test_tl != null_tl );
 template<typename... T1, typename... T2>
 constexpr auto operator+(type_list<T1...>, type_list<T2...>)
 { 
-  ---
+  return type_list<T1..., T2...>{};
 }
 
 static_assert( (null_tl + null_tl) == null_tl );
@@ -109,10 +103,13 @@ static_assert( (test_tl + test_tl) ==
 // d'une "type_list"
 //==============================================================================
 
-template<--->
---- largest(---)
-{
-  ---
+template<typename... T>
+constexpr std::size_t largest(type_list<T...>) {
+  if constexpr (sizeof...(T) == 0) { // sans constexpr, on aurait une erreur de compilation à cause de l'appel à std::max avec une liste vide
+    return 0;
+  } else {
+    return std::max({sizeof(T)...});
+  }
 }
 
 static_assert(largest(null_tl) == 0 );
@@ -124,11 +121,18 @@ static_assert(largest(test_tl) == 19);
 // identiques au type "T" passé en template
 //==============================================================================
 
-template<--->
---- all_of(---)
-{
-  ---
+template<typename T, typename... U>
+constexpr bool all_of(type_list<U...>) {
+  return (std::is_same_v<T, U> && ...); // on applique le prédicat pour chaque élément
 }
+
+// https://en.cppreference.com/cpp/language/fold: 
+// bool all(Args... args) { return (... && args); }
+// bool b = all(true, true, true, false);
+// // within all(), the unary left fold expands as
+// //  return ((true && true) && true) && false;
+// // b is false
+
 
 static_assert(!all_of<int>(type_list<double>{}));
 static_assert(!all_of<int>(test_tl));
